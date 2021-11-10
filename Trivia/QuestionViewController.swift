3365662602c106c608df5306e8851fc5d529a48a
Mauro Alvarenga <mauro.e.alvarenga@gmail.com>
2021-11-10 20:11:55 -0300

    refactor to MVVM

--- Trivia/QuestionViewController.swift
+++ Trivia/QuestionViewController.swift
@@ -13,130 +13,95 @@ class QuestionViewController: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     
-    private var questions = Contenido.shared.getQuestions()
-    private var currentQuestionIndex = 0
+    private var viewModel: QuestionViewModel!
     private var username: String?
     private var score: Int?
+    private var currentQuestion: Question?
     
     var categoryID = 0
-    private var currentQuestion: Question?
-    private let questionsService = QuestionsService()
-    
     let userDefaults = UserDefaults()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.viewModel = QuestionViewModel(service: QuestionsService())
         view.backgroundColor = UIColor(patternImage: UIImage(named: "background_1")!)
-        //setCurrentQuestion(for: currentQuestionIndex)
-        //setUsername()
-        if let usernameSet = userDefaults.string(forKey: "username") {
-            self.username = usernameSet
-            self.nameLabel.text = usernameSet + ":"
-        } else {
-            self.username = "Jugador:"
-        }
+        setUsername()
+        getScore()
         getQuestion()
-        //setRandomQuestion()
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        //setScore()
-        self.score = userDefaults.integer(forKey: "score")
-        self.scoreLabel.text = "Score: " + String(self.score!)
+        getScore()
     }
     
+    private func getQuestion() {
+        viewModel.getQuestion(for: categoryID) { [weak self] in
+            guard let strongSelf = self else { return }
+            strongSelf.setCurrentQuestion()
+        }
+    }
+
     @IBAction func yesButtonTapped(_ sender: UIButton) {
-        //let result = validateCurrentQuestion(answer: true)
         let result = validateCurrentQuestion(answer: "True")
         sendResultAlert(for: result)
     }
     
     @IBAction func noButtonTapped(_ sender: UIButton) {
-       // let result = validateCurrentQuestion(answer: false)
         let result = validateCurrentQuestion(answer: "False")
         sendResultAlert(for: result)
     }
-    
-    private func getQuestion() {
-        questionsService.getQuestion(for: categoryID) { [weak self] receivedQuestion in
-            guard let strongSelf = self else { return }
-            
-            strongSelf.currentQuestion = receivedQuestion
-            strongSelf.questionLabel.text = receivedQuestion.question.htmlDecoded
-        }
+     
+    private func setCurrentQuestion() {
+        questionLabel.text = viewModel.getCurrentQuestion()
     }
-    
-//    private func setRandomQuestion() {
-//        questionsService.getRandomQuestion { [weak self] receivedQuestion in
-//            //if receivedQuestions.count > 0 {
-//            print(receivedQuestion)
-//            self?.currentQuestion = receivedQuestion
-//            self?.questionLabel.text = receivedQuestion.question.htmlDecoded
-//            //}
-//        }
-//    }
     
     private func validateCurrentQuestion(answer: String) -> Bool {
-        //questions[currentQuestionIndex].answer == answer
-        if let rightAnswer = currentQuestion?.correct_answer {
-            return rightAnswer == answer
-        }
-        return false
+        viewModel.validateCurrentQuestion(answer: answer)
     }
-    
+        
     private func sendResultAlert(for result: Bool) {
         result ? rightAnswerTapped() : wrongAnswerTapped()
     }
-    
-    func rightAnswerTapped() {
+   
+    private func rightAnswerTapped() {
         let alertYES = UIAlertController(title: "Excellent!", message: "Good Job, \(username!)! 😁", preferredStyle: .alert)
         alertYES.addAction(UIAlertAction(title: "Thanks! 😎", style: .cancel, handler: { [self] _ in
-            NSLog("The \"correct answer\" alert occured.")
-            //updateQuestion()
-            //setRandomQuestion()
-            self.score! += 5
-            self.scoreLabel.text = "Score: " + String(self.score!)
-            userDefaults.set(self.score!, forKey: "score")
+            increaseScore()
             getQuestion()
         }))
         self.present(alertYES, animated: true)
         }
     
-    func wrongAnswerTapped() {
+    private func wrongAnswerTapped() {
         let alertNO = UIAlertController(title: "Wrong!", message: "Better luck next time, \(username!) 😔", preferredStyle: .alert)
         alertNO.addAction(UIAlertAction(title: "Ups! 😅", style: .cancel, handler: { [self] _ in
-            NSLog("The \"correct answer\" alert occured.")
-            //updateQuestion()
-            //setRandomQuestion()
             getQuestion()
         }))
         self.present(alertNO, animated: true)
     }
     
-    private func updateQuestion() {
-        currentQuestionIndex += 1
-        setCurrentQuestion(for: currentQuestionIndex)
-    }
-    
-    private func setCurrentQuestion(for index: Int) {
-        if index < questions.count {
-            questionLabel.text = questions[index].question
-        } else {
-            currentQuestionIndex = 0
-            questionLabel.text = questions[currentQuestionIndex].question
+    private func setUsername() {
+        if let usernameSet = userDefaults.string(forKey: "username") {
+            self.username = usernameSet
+            self.nameLabel.text = usernameSet + ":"
         }
     }
-        
-}
-
-// Used to decode special characters like '&quot;' from the JSON
-extension String {
-    var htmlDecoded: String {
-        let decoded = try? NSAttributedString(data: Data(utf8), options: [
-            .documentType: NSAttributedString.DocumentType.html,
-            .characterEncoding: String.Encoding.utf8.rawValue
-        ], documentAttributes: nil).string
-
-        return decoded ?? self
+    
+    private func getScore() {
+        self.score = userDefaults.integer(forKey: "score")
+        setScore()
     }
+    
+    private func setScore() {
+        if let score = self.score {
+            self.scoreLabel.text = "Score: " + String(score)
+        }
+    }
+    
+    private func increaseScore() {
+        self.score! += 5
+        userDefaults.set(self.score!, forKey: "score")
+        setScore()
+    }
+    
 }
